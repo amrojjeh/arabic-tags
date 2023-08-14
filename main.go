7261c11b71643793b5a23ff6d96ad6d0c9e8aff8
Amr Ojjeh <amrojjeh@outlook.com>
2023-08-14 12:37:16 -0500

    Some experiment

--- main.go
+++ main.go
@@ -2,9 +2,49 @@ package main
 
 import (
 	"net/http"
-	"time"
 	"os"
+	"time"
 )
+
+type SpeechType int
+
+type Function int
+
+type Case struct {
+	class     CaseClass
+	explicit  bool
+	indicator string
+	index int // the index in which the case is explicit within the word
+}
+
+type CaseClass int
+
+const (
+	Noun     SpeechType = iota // اسم
+	Verb                       // فعل
+	Particle                   // حرف
+)
+
+const (
+	Subject Function = iota
+	Object
+)
+
+const (
+	NA CaseClass = iota // not applicable
+	Nominative
+	Accusative
+	Genetive
+	// Add case for jazm
+)
+
+type Word struct {
+	value    string
+	speech   SpeechType
+	function Function
+	cas      Case // so that it doesn't conflict with case; also how it's named in CAMeL tools
+	tokens   []int
+}
 
 func main() {
 	mux := http.NewServeMux()
