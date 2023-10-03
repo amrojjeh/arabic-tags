d2b543fa153bb2db697621ebd88430033cb8b23e
Amr Ojjeh <amrojjeh@outlook.com>
2023-10-03 16:58:26 -0500

    Add schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS arabic_tags;
+
+CREATE TABLE IF NOT EXISTS arabic_tags.excerpt (
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    grammar JSON NOT NULL,
+    technical JSON NOT NULL,
+    created DATETIME NOT NULL,
+    updated DATETIME NOT NULL
+);
+
