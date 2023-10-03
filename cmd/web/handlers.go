94497b978eb2758bcdacdb0338a3c103448ece07
Amr Ojjeh <amrojjeh@outlook.com>
2023-10-03 12:53:06 -0500

    Add initial save

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/amrojjeh/arabic-tags/internal/models"
+	"github.com/google/uuid"
 )
 
 // TODO(Amr Ojjeh): Return error if there's no id param
 func (app *application) excerptGet(w http.ResponseWriter, r *http.Request) {
-	app.renderTemplate(w, "add.tmpl", http.StatusOK, templateData{})
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	idStr := r.Form.Get("id")
+	if idStr == "" {
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+		return
+	}
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	excerpt, err := app.excerpts.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			// TODO(Amr Ojjeh): Write an excerpt not found page
+			w.Write([]byte("Excerpt not found..."))
+			return
+		}
+		app.serverError(w, err)
+		return
+	}
+	data := templateData{
+		Excerpt: excerpt,
+	}
+	app.renderTemplate(w, "add.tmpl", http.StatusOK, data)
 }
 
 type excerptForm struct {
@@ -48,5 +83,12 @@ func (app *application) excerptCreatePost(w http.ResponseWriter, r *http.Request
 	}
 
 	// TODO(Amr Ojjeh): Insert and then redirect with proper id
-	http.Redirect(w, r, "/excerpt", http.StatusSeeOther)
+	id, err := app.excerpts.Insert(form.Title)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	idStr := strings.ReplaceAll(id.String(), "-", "")
+	http.Redirect(w, r, fmt.Sprintf("/excerpt?id=%v", idStr), http.StatusSeeOther)
 }
--- internal/models/excerpt.go
+++ internal/models/excerpt.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"database/sql"
+	"encoding/json"
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,7 +12,29 @@ import (
 type Grammar struct {
 }
 
+func (g *Grammar) Scan(src any) error {
+	switch src.(type) {
+	case []byte:
+		json.Unmarshal(src.([]byte), g)
+	default:
+		return errors.New("grammar: cannot scan type")
+	}
+
+	return nil
+}
+
 type Technical struct {
+}
+
+func (t *Technical) Scan(src any) error {
+	switch src.(type) {
+	case []byte:
+		json.Unmarshal(src.([]byte), t)
+	default:
+		return errors.New("technical: cannot scan type")
+	}
+
+	return nil
 }
 
 type Excerpt struct {
@@ -27,9 +51,31 @@ type ExcerptModel struct {
 	DB *sql.DB
 }
 
+func (m ExcerptModel) Get(id uuid.UUID) (Excerpt, error) {
+	stmt := `SELECT title, content, grammar, technical, created, updated
+	FROM excerpt WHERE excerpt.id = UUID_TO_BIN(?)`
+
+	var e Excerpt
+	e.ID = id
+	idVal, err := id.Value()
+	if err != nil {
+		return e, err
+	}
+	err = m.DB.QueryRow(stmt, idVal).Scan(&e.Title, &e.Content,
+		&e.Grammar, &e.Technical, &e.Created, &e.Updated)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return e, ErrNoRecord
+		}
+		return e, err
+	}
+
+	return e, nil
+}
+
 func (m ExcerptModel) Insert(title string) (uuid.UUID, error) {
 	stmt := `INSERT INTO excerpt (id, title, content, grammar, technical,
-	created, updated) VALUES (?, ?, "", "{}", "{}", UTC_TIMESTAMP(),
+	created, updated) VALUES (UUID_TO_BIN(?), ?, "", "{}", "{}", UTC_TIMESTAMP(),
 	UTC_TIMESTAMP())`
 
 	// Technically it's not good practice to use UUIDv4 as a PK,
@@ -40,8 +86,11 @@ func (m ExcerptModel) Insert(title string) (uuid.UUID, error) {
 	// We're also unlikely to run into perf issues since we're operating on a
 	// small scale.
 	id := uuid.New()
-
-	_, err := m.DB.Exec(stmt, [16]byte(id), title)
+	idVal, err := id.Value()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	_, err = m.DB.Exec(stmt, idVal, title)
 	if err != nil {
 		return uuid.UUID{}, err
 	}
