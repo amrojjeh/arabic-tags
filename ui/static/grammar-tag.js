725f27b7779941b66fe29e9d05752fbfe5fde11b
Amr Ojjeh <amrojjeh@outlook.com>
2023-10-09 11:10:14 -0500

    Add tags

--- ui/static/grammar-tag.js
+++ ui/static/grammar-tag.js
@@ -1,5 +1,6 @@
 const html = String.raw;
 
+// TODO(Amr Ojjeh): Get the list for the tagssssssss
 export class GrammarTag extends HTMLElement {
   constructor() {
     super();
@@ -14,12 +15,14 @@ export class GrammarTag extends HTMLElement {
     this._initHTML();
     document.body.addEventListener("keydown", this._keydown);
     this.HTML.input.addEventListener("keydown", this._keydown);
+    this.HTML.input.addEventListener("keyup", this._keyup);
     this.render();
   }
 
   disconnectedCallback() {
     document.body.removeEventListener("keydown", this._keydown);
     this.HTML.input.removeEventListener("keydown", this._keydown);
+    this.HTML.input.removeEventListener("keyup", this._keyup);
   }
 
   selectPrev() {
@@ -84,6 +87,14 @@ export class GrammarTag extends HTMLElement {
         this.HTML.p.append(" ");
       }
     }
+
+    const wordObj = this.data.words[this.data.selectedIndex];
+    this.HTML.tagContainer.innerHTML = "";
+    for (let i = 0; i < wordObj.tags.length; ++i) {
+      const tag = wordObj.tags[i];
+      const partial = this.tagPartial(tag);
+      this.HTML.tagContainer.appendChild(partial)
+    }
   }
 
   wordPartial(index) {
@@ -96,6 +107,20 @@ export class GrammarTag extends HTMLElement {
     }
     frag.appendChild(span);
     return frag;
+  }
+
+  tagPartial(tagValue) {
+    const frag = document.createDocumentFragment();
+    const li = document.createElement("li");
+    li.innerText = tagValue;
+    frag.appendChild(li);
+    return frag;
+  }
+
+  addTag(index, tag) {
+    const wordObj = this.data.words[index];
+    wordObj.tags.push(tag);
+    this.render();
   }
 
   genWord(word) {
@@ -116,12 +141,13 @@ export class GrammarTag extends HTMLElement {
   }
 
   _initHTML() {
-    // TODO(Amr Ojjeh): div > input because we want to add suggestions as well
     this.innerHTML = html`
       <div dir="rtl" class="py-10 px-2 h-full">
         <p class="ps-3 pe-3 text-4xl leading-loose">${this.data.value}</p>
-        <div class="pt-10 flex flex-col gap-5">
-          <input autofocus placeholder="اكتب..." type="text" class="text-3xl ps-2 py-2 leading-loose drop-shadow mx-auto"></input>
+        <div class="pt-10 flex flex-col gap-5 mx-auto w-1/2">
+          <input autofocus placeholder="اكتب..." type="text" class="text-3xl ps-2 py-2 leading-loose drop-shadow"></input>
+          <ul id="tag-container" class="text-3xl list-disc marker:text-red-800 list-inside leading-loose">
+          </ul>
         </div>
       </div>`;
 
@@ -129,25 +155,37 @@ export class GrammarTag extends HTMLElement {
     this.HTML.root = this.querySelector("div");
     this.HTML.p = this.querySelector("div > p");
     this.HTML.input = this.querySelector("input");
+    this.HTML.tagContainer = this.querySelector("#tag-container");
   }
 
+  _keyup = (e) => {
+    if (e.key === "Enter") {
+      this.addTag(this.data.selectedIndex, this.HTML.input.value);
+      this.HTML.input.value = "";
+    }
+  }
   _keydown = (e) => {
-    const keys = ["[", "]", "{", "}"];
+    const keys = ["Home", "End"];
     if (keys.indexOf(e.key) !== -1) {
       e.preventDefault();
       e.stopPropagation();
       switch (e.key) {
-        case "{":
-          this.expandSelection();
-          break;
-        case "}":
-          this.shrinkSelection();
-          break;
-        case "[":
+        case "Home":
+          if (e.shiftKey) {
+            this.expandSelection();
+            break;
+          }
           this.selectNext();
           break;
-        case "]":
+        case "End":
+          if (e.shiftKey) {
+            this.shrinkSelection();
+            break;
+          }
           this.selectPrev();
+          break;
+        default:
+          console.error("Should not happen");
           break;
       }
     }
