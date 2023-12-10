c49bc36d84c6da3bd6dd578b510be946857e5de3
Amr Ojjeh <amrojjeh@outlook.com>
2023-12-10 12:39:01 -0600

    Test disambiguator

---
+++ cmd/disambig/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/amrojjeh/arabic-tags/internal/speech"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		fmt.Println("Input error!")
+		return
+	}
+	text, _ = strings.CutSuffix(text, "\n")
+	words, err := speech.Disambiguate(text)
+	if err != nil {
+		if errors.Is(err, speech.ErrRequest) {
+			fmt.Printf("%v\n", text)
+			return
+		}
+		fmt.Printf("Error! %v\n", err.Error())
+		return
+	}
+
+	for _, w := range words {
+		fmt.Printf("%v ", w)
+	}
+	fmt.Println()
+}
